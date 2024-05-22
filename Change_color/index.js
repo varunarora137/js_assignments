1633420c367b6fa2467c41f17600c7bdec8fd757
Varun Arora <varunarora137@gmail.com>
2024-05-23 03:29:10 +0530

    modified change_color

---
+++ Change_color/index.js
@@ -0,0 +1,42 @@
+const circle = document.querySelector(".circle");
+const shape = document.querySelector(".shape");
+const color = document.querySelector(".color");
+
+const colorOptions = [
+  "#008000",
+  "#EDADBC",
+  "#79E388",
+  "#075B60",
+  "#15718D",
+  "#8955B5",
+  "#3BEA35",
+  "#CB2876",
+  "#9C1CCB",
+  "#3D142B",
+  "#8C70D7",
+];
+
+const shapeOption = [
+  "square",
+  "heptagon",
+  "bevel",
+  "rectangle",
+  "rightpoint",
+  "leftpoint",
+  "star",
+  "rightarrow",
+  "leftarrow",
+  "close",
+  "message",
+];
+
+color.addEventListener("click", () => {
+  const num = Math.trunc(Math.random() * 11);
+  circle.style.backgroundColor = `${colorOptions[num]}`;
+  console.log(num);
+});
+
+shape.addEventListener("click", () => {
+  const num = Math.trunc(Math.random() * 11);
+  circle.children[0].id = `${shapeOption[num]}`;
+});
