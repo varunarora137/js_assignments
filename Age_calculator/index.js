9d8f870806f5cb8b815d7860fedf1cc98365598b
Varun Arora <varunarora137@gmail.com>
2024-05-23 18:17:43 +0530

    modified age_calculator

--- Age_calculator/index.js
+++ Age_calculator/index.js
@@ -17,7 +17,7 @@ btn.addEventListener("click", () => {
   const currYear = currDate.getFullYear();
   const currMonth = currDate.getMonth() + 1;
 
-  const yearDiff = currYear - inputYear;
+  let yearDiff = currYear - inputYear;
   const monthDiff = currMonth - inputMonth;
   const dateDiff = currDat - inputDat;
 
@@ -28,6 +28,9 @@ btn.addEventListener("click", () => {
   ) {
     alert("Not Born Yet");
     return;
+  }
+  if (monthDiff < 0 || (monthDiff === 0 && dateDiff < 0)) {
+    yearDiff--;
   }
 
   divs.children[0].children[0].innerText = `${Math.abs(yearDiff)}`;
