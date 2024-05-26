50d649a902a7c8b4c7ba89e801869f82956e676c
Varun Arora <varunarora137@gmail.com>
2024-05-27 04:58:24 +0530

    modified keyboard_value

--- keyboard_value/index.js
+++ keyboard_value/index.js
@@ -6,4 +6,6 @@ window.addEventListener("keyup", (e) => {
   code_div.style.display = "flex";
   text_div.innerHTML = `You pressed&nbsp<span class="key">${e.key}</span>`;
   code_div.innerText = `${e.keyCode}`;
+  const sound = new Audio("./beep.wav");
+  sound.play();
 });
