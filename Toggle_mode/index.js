69fa2ba8b7fef78bf4e0470e287c2ff9fbadd77e
Varun Arora <varunarora137@gmail.com>
2024-05-27 03:41:58 +0530

    modified toggle_mode

--- Toggle_mode/index.js
+++ Toggle_mode/index.js
@@ -6,16 +6,18 @@ const moon = document.querySelector(".moon");
 
 toggle_div.addEventListener("click", () => {
   if (circle.classList.contains("moveRight")) {
+    sun.classList.remove("sun-down");
+    moon.classList.remove("moon-up");
     circle.classList.remove("moveRight");
     document.body.style.backgroundColor = "white";
     text.style.color = "black";
-    moon.style.opacity = "0";
-    sun.style.opacity = "1";
+    // sun.style.opacity = "1";
   } else {
     circle.classList.add("moveRight");
     document.body.style.backgroundColor = "black";
     text.style.color = "White";
-    sun.style.opacity = "0";
-    moon.style.opacity = "1";
+    // sun.style.opacity = "0";
+    moon.classList.add("moon-up");
+    sun.classList.add("sun-down");
   }
 });
