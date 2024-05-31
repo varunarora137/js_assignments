697b9f9e44d2c6817161103f1b564841bfdb2047
Varun Arora <varunarora137@gmail.com>
2024-05-31 18:28:02 +0530

    modified emoji-project

--- emoji-project/index.js
+++ emoji-project/index.js
@@ -4,26 +4,30 @@ const search_btn = document.querySelector(".srch");
 function searchEmoji(e) {
   e.preventDefault();
   const txt = text_search.value;
-  console.log(txt);
   showEmoji(txt);
 }
 function autoSearch() {
   const txt = text_search.value;
-  console.log(txt);
   showEmoji(txt);
 }
-
+let ct = 0;
 function showEmoji(searchText = "") {
   const emojiArr = emojiList.filter((emj) => {
     const matchText = emj.tags.join(" ").includes(searchText);
     if (matchText === true) return true;
   });
-  console.log(emojiArr);
   const emojee_container = document.querySelector(".emojee-container");
   emojee_container.innerHTML = "";
   emojiArr.forEach((emo) => {
     const new_div = document.createElement("div");
     new_div.classList.add("emojee");
+    if (ct % 2 === 0) {
+      new_div.classList.add("animation-left");
+      new_div.style.backgroundColor = "rgb(92, 105, 101)";
+    } else {
+      new_div.classList.add("animation-right");
+    }
+    ct++;
     new_div.innerHTML = `<div class="img-title">
             <div class="img">${emo.emoji}</div>
             <div class="title">${emo.aliases
