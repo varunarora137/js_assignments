34aa3ad0611824caab5308df94107fd8a3ede972
Varun Arora <varunarora137@gmail.com>
2024-06-01 19:24:37 +0530

    added digital-clock

---
+++ digital-clock/index.js
@@ -0,0 +1,31 @@
+const hour = document.querySelector(".hour");
+const minute = document.querySelector(".minute");
+const second = document.querySelector(".second");
+const night = document.querySelector(".night");
+const line3 = document.querySelector(".line-3");
+
+function getTime() {
+  const date = new Date();
+  let hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  let pm = false;
+  if (hours >= 12) {
+    hours = hours - 12;
+    pm = true;
+  } else pm = false;
+  hour.innerText = `${String(hours).padStart(2, "0")}`;
+  minute.innerHTML = `${String(minutes).padStart(2, "0")}`;
+  second.innerText = `${String(seconds).padStart(2, "0")}`;
+  night.innerText = `${pm && "PM"}`;
+
+  const secOffset = 1075 - seconds * 17.9; // 1075/60=17.9
+  line3.style.strokeDashoffset = secOffset;
+
+  const minOffset = 1306 * (1 - minutes / 60);
+  document.querySelector(".line-2").style.strokeDashoffset = minOffset;
+
+  const hourOffset = 1538 * (1 - hours / 12);
+  document.querySelector(".line-1").style.strokeDashoffset = hourOffset;
+}
+setInterval(getTime, 1000);
