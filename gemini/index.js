7d43bd55b0139faa060bc17c8964d615dc269ee7
Varun Arora <varunarora137@gmail.com>
2024-06-07 23:18:59 +0530

    modified gemini clone

--- gemini/index.js
+++ gemini/index.js
@@ -1,18 +1,18 @@
-import { GoogleGenerativeAI } from "@google/generative-ai";
+// import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const API_KEY = "AIzaSyDw18JFSzEgbPcK0MOO8SnQPQ_xvWhU6Gg";
+// const API_KEY = "AIzaSyDw18JFSzEgbPcK0MOO8SnQPQ_xvWhU6Gg";
 
-const genAI = new GoogleGenerativeAI(API_KEY);
+// const genAI = new GoogleGenerativeAI(API_KEY);
 
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+// const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-async function run() {
-  const prompt = "Write a story about a magic backpack.";
+// async function run() {
+//   const prompt = "Write a story about a magic backpack.";
 
-  const result = await model.generateContent("prompt");
-  const response = await result.response;
-  const text = response.text();
-  console.log(text);
-}
+//   const result = await model.generateContent("prompt");
+//   const response = await result.response;
+//   const text = response.text();
+//   console.log(text);
+// }
 
-run();
+// run();
